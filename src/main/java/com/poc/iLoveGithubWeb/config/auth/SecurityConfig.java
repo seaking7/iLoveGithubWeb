3c7af24d3d19caf8b5eb2b97be72b83df5a89b96
taekyungKim <seaking7@gmail.com>
2022-11-17 10:07:07 +0900

    jasypt를 이용한 config 암호화

--- src/main/java/com/poc/iLoveGithubWeb/config/auth/SecurityConfig.java
+++ src/main/java/com/poc/iLoveGithubWeb/config/auth/SecurityConfig.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
@@ -33,4 +35,5 @@ public class SecurityConfig {
                             .userService(customOAuth2UserService);      //oauth로그인 성공시 후속조치
         return http.build();
     }
+
 }
